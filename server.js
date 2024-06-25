abb507a411218376590817359ff0e47357bf6eae
LarryYapper <filipkubik.mail@gmail.com>
2024-06-25 22:29:58 +0200

    finaly, hopefuly, this is the last commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static());
 
 const soundsDir = path.join(__dirname, 'sounds');
 const checkedDir = path.join(__dirname, 'Sounds_checked');
